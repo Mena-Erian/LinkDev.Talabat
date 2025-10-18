988d3c0011721f00856e160e68deff17807cac11
Mena Erian <mena3koko2karas1@gmail.com>
2025-10-18 21:13:10 +0300

    Product Module - Entities

---
+++ LinkDev.Talabat.Domain/Entities/Products/Product.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Domain.Entities.Products.Products;
+
+namespace LinkDev.Talabat.Domain.Entities.Products
+{
+    public class Product : BaseAuditableEntity<string>
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+
+        public int? CategoryId { get; set; }
+        public virtual ProductCategory? Category { get; set; }
+        public int? BrandId { get; set; }
+        public virtual ProductBrand? Brand { get; set; }
+    }
+
+}
